74a1bc66344db4f515a70d2f5a40c93a8a2e999d
Choi-U <lyj64842999@gmail.com>
2022-11-15 22:23:49 +0900

    d123

--- mytest.py
+++ mytest.py
@@ -1,5 +1,10 @@
-a=input()
-b=input()
-a=int(a)
-b=int(b)
-print(a*b)
+a,b=map(int, input().split())
+
+if a>0 and b>0:
+    print('1')
+elif a<0 and b>0:
+    print('2')
+elif a<0 and b>0:
+    print('3')  
+elif a>0 and b<0:
+    print('4')
